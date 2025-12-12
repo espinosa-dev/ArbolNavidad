1670eff279af5d340e4724e905907458dc26291f
espinosa-dev <alvaroespinosamontesinos@gmail.com>
2025-12-12 16:19:49 +0100

    Added app.bat for console execution

---
+++ src/app.bat
@@ -0,0 +1,11 @@
+@echo off
+title Arbol Navidad Animado
+cls
+
+echo Iniciando el arbol...
+echo ------------------------------
+
+java -jar "F:\Programación\Java\ArbolNavidad\out\production\ArbolNavidad\Animated.jar" Animated.jar 3 s s s s
+
+echo ------------------------------
+pause
